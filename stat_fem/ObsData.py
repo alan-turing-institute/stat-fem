7fc1d320bce47cab64944cc723ab25cab931363d
edaub <edaub@turing.ac.uk>
2019-11-18 12:11:40 +0000

    wrote basic class for observational data

---
+++ stat_fem/ObsData.py
@@ -0,0 +1,80 @@
+import numpy as np
+
+class ObsData(object):
+    "represents a set of observations made at a set of coordinates with a corresponding uncertainty"
+    def __init__(self, coords, data, unc):
+        """
+        create a new data object from locations, observations, and uncertainties
+
+        coords is a 1D or 2D array holding the spatial locations of the measurements
+             if 1D, then assumes that FEM is explicitly 1D in nature. If 2D, then
+             FEM can be 1D, 2D, or 3D with the second index
+        data is the measurements itself, a 1D numpy array with the same length as the
+             first dimension of coords
+        unc is the measurement error (as a standard deviation), assumed to be independent
+             for each measurement can be either an array of the same length as data or
+             a single float that is applied to all numbers. None or NaN is assumed to be zero
+        """
+
+        coords = np.array(coords, dtype=np.float64)
+
+        if coords.ndim == 1:
+            coords = np.reshape(coords, (-1, 1))
+
+        assert coords.ndim == 2, "coords must be a 1D or 2D array"
+
+        self.n_obs = coords.shape[0]
+        self.n_dim = coords.shape[1]
+        self.coords = np.copy(coords)
+
+        data = np.array(data, dtype=np.float64)
+        assert data.shape == (self.n_obs,), "data must be a 1D array with the same length as coords"
+        self.data = np.copy(data)
+
+        unc = np.array(unc, dtype=np.float64)
+        unc = np.nan_to_num(unc)
+        assert unc.shape == (self.n_obs,) or unc.shape == (), "bad shape for unc, must be an array or float"
+        assert np.all(unc >= 0.), "all uncertainties must be non-negative"
+        self.unc = np.copy(unc)
+
+    def get_n_dim(self):
+        "returns number of dimensions in FEM model"
+
+        return self.n_dim
+
+    def get_n_obs(self):
+        "returns number of observations"
+
+        return self.n_obs
+
+    def get_coords(self):
+        "returns coordinate points as a numpy array"
+
+        return self.coords
+
+    def get_data(self):
+        "returns observations"
+
+        return self.data
+
+    def get_unc(self):
+        "returns uncertainties (as standard deviation)"
+
+        return self.unc
+
+    def __str__(self):
+        "returns a string representation"
+
+        outstr = ("Observational Data:\n" +
+                  "Number of dimensions:\n" +
+                  "{}\n".format(self.get_n_dim()) +
+                  "Number of observations:\n" +
+                  "{}\n".format(self.get_n_obs()) +
+                  "Coordinates:\n" +
+                  "{}\n".format(self.get_coords()) +
+                  "Data:\n" +
+                  "{}\n".format(self.get_data()) +
+                  "Uncertainty:\n" +
+                  "{}".format(self.get_unc()))
+
+        return outstr
---
+++ stat_fem/tests/test_ObsData.py
@@ -0,0 +1,163 @@
+import pytest
+import numpy as np
+from numpy.testing import assert_allclose
+from ..ObsData import ObsData
+
+def test_ObsData_init():
+    "test ObsData"
+
+    # simple 1D coords and single uncertainty
+
+    coords = np.array([1., 2., 3.])
+    data = np.array([2., 3., 4.])
+    unc = 1.
+
+    od = ObsData(coords, data, unc)
+
+    assert_allclose(od.coords, np.reshape(coords, (-1, 1)))
+    assert_allclose(od.data, data)
+    assert_allclose(od.unc, unc)
+    assert od.n_dim == 1
+    assert od.n_obs == 3
+
+    # 2D coords and array uncertainties
+
+    coords = np.array([[1., 2.], [3., 4.], [5., 6.]])
+    unc = np.array([4., 5., 6.])
+
+    od = ObsData(coords, data, unc)
+
+    assert_allclose(od.coords, coords)
+    assert_allclose(od.data, data)
+    assert_allclose(od.unc, unc)
+    assert od.n_dim == 2
+    assert od.n_obs == 3
+
+    # unc is none
+
+    unc = None
+
+    od = ObsData(coords, data, unc)
+
+    assert_allclose(od.coords, coords)
+    assert_allclose(od.data, data)
+    assert_allclose(od.unc, 0.)
+    assert od.n_dim == 2
+    assert od.n_obs == 3
+
+def test_ObsData_init_failures():
+    "check situations where ObsData should fail"
+
+    # bad shape for coords
+
+    coords = np.array([[[1., 2., 3.]]])
+    data = np.array([3., 4.])
+    unc = 1.
+
+    with pytest.raises(AssertionError):
+        od = ObsData(coords, data, unc)
+
+    # shape of coords and data don't agree
+
+    coords = np.array([1., 2., 3.])
+    data = np.array([3., 4.])
+    unc = 1.
+
+    with pytest.raises(AssertionError):
+        od = ObsData(coords, data, unc)
+
+    # bad shape for unc
+
+    coords = np.array([1., 2., 3.])
+    data = np.array([2., 3., 4.])
+    unc = np.array([1., 2.])
+
+    with pytest.raises(AssertionError):
+        od = ObsData(coords, data, unc)
+
+    # negative uncertainty
+
+    coords = np.array([1., 2., 3.])
+    data = np.array([2., 3., 4.])
+    unc = -1.
+
+    with pytest.raises(AssertionError):
+        od = ObsData(coords, data, unc)
+
+def test_ObsData_get_n_dim():
+    "test the get_n_dim method of ObsData"
+
+    coords = np.array([[1., 2.], [3., 4.], [5., 6.]])
+    data = np.array([1., 2., 3.])
+    unc = np.array([4., 5., 6.])
+
+    od = ObsData(coords, data, unc)
+
+    assert od.get_n_dim() == 2
+
+def test_ObsData_get_n_obs():
+    "test the get_n_obs method of ObsData"
+
+    coords = np.array([[1., 2.], [3., 4.], [5., 6.]])
+    data = np.array([1., 2., 3.])
+    unc = np.array([4., 5., 6.])
+
+    od = ObsData(coords, data, unc)
+
+    assert od.get_n_obs() == 3
+
+def test_ObsData_get_coords():
+    "test the get_coords method of ObsData"
+
+    coords = np.array([[1., 2.], [3., 4.], [5., 6.]])
+    data = np.array([1., 2., 3.])
+    unc = np.array([4., 5., 6.])
+
+    od = ObsData(coords, data, unc)
+
+    assert_allclose(od.get_coords(), coords)
+
+def test_ObsData_get_data():
+    "test the get_data method of ObsData"
+
+    coords = np.array([[1., 2.], [3., 4.], [5., 6.]])
+    data = np.array([1., 2., 3.])
+    unc = np.array([4., 5., 6.])
+
+    od = ObsData(coords, data, unc)
+
+    assert_allclose(od.get_data(), data)
+
+def test_ObsData_get_unc():
+    "test the get_unc method of ObsData"
+
+    coords = np.array([[1., 2.], [3., 4.], [5., 6.]])
+    data = np.array([1., 2., 3.])
+    unc = np.array([4., 5., 6.])
+
+    od = ObsData(coords, data, unc)
+
+    assert_allclose(od.get_unc(), unc)
+
+def test_ObsData_str():
+    "test the string method of ObsData"
+
+    coords = np.array([[1., 2.], [3., 4.], [5., 6.]])
+    data = np.array([1., 2., 3.])
+    unc = np.array([4., 5., 6.])
+
+    od = ObsData(coords, data, unc)
+
+    outstr = ("Observational Data:\n" +
+              "Number of dimensions:\n" +
+              "{}\n".format(2) +
+              "Number of observations:\n" +
+              "{}\n".format(3) +
+              "Coordinates:\n" +
+              "{}\n".format(coords) +
+              "Data:\n" +
+              "{}\n".format(data) +
+              "Uncertainty:\n" +
+              "{}".format(unc))
+
+    assert outstr == str(od)
