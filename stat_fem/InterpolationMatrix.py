d3c06f5788af7882f798249488edd16075eaba28
edaub <edaub@turing.ac.uk>
2019-12-02 15:16:50 +0000

    implemented parallel version of interpolation matrix

--- stat_fem/InterpolationMatrix.py
+++ stat_fem/InterpolationMatrix.py
@@ -1,37 +1,172 @@
+import sys
 import numpy as np
-from firedrake import Function
+from firedrake import Function, COMM_WORLD
+from firedrake.ensemble import Ensemble
 from firedrake.petsc import PETSc
 from firedrake.functionspaceimpl import WithGeometry
 
 class InterpolationMatrix(object):
     "class representing an interpolation matrix"
-    def __init__(self, coords, function_space):
+    def __init__(self, coords, function_space, ensemble_comm=None):
         "create and assemble interpolation matrix"
+
+        if ensemble_comm is None or ensemble_comm == COMM_WORLD:
+            # if no ensemble communicator, do not parallelize forcing covariance solving
+            self.ensemble_comm = Ensemble(COMM_WORLD, COMM_WORLD.size)
+        else:
+            if not isinstance(ensemble_comm, Ensemble):
+                raise (TypeError, "ensemble_comm must be an Ensemble or COMM_WORLD")
+            self.ensemble_comm = ensemble_comm
 
         if not isinstance(function_space, WithGeometry):
             raise TypeError("bad input type for function_space: must be a FunctionSpace")
 
+        self.coords = np.copy(coords)
+        self.function_space = function_space
+
         self.n_data = coords.shape[0]
-        assert (coords.shape[1] == function_space.mesh().cell_dimension()
+        assert (coords.shape[1] == self.function_space.mesh().cell_dimension()
                 ), "shape of coordinates does not match mesh dimension"
-        self.n_mesh = function_space.mesh().num_vertices()
+
+        # allocate working vectors to handle parallel matrix operations and data transfer
+
+        # dataspace_vector is a distributed PETSc vector in the data space
+
+        self.dataspace_distrib = PETSc.Vec().create(comm=self.ensemble_comm.comm)
+        self.dataspace_distrib.setSizes((-1, self.n_data))
+        self.dataspace_distrib.setFromOptions()
+
+        self.n_data_local = self.dataspace_distrib.getSizes()[0]
+
+        # all data computations are done on root process, so create gather method to
+        # facilitate this data transfer
+
+        self.petsc_scatter, self.dataspace_gathered = PETSc.Scatter.toZero(self.dataspace_distrib)
+
+        self.meshspace_vector = Function(self.function_space).vector()
+
+        self.n_mesh_local = self.meshspace_vector.local_size()
+        self.n_mesh = self.meshspace_vector.size()
 
         nnz = len(function_space.cell_node_list[0])
 
-        self.interp = PETSc.Mat().createAIJ((self.n_data, self.n_mesh), nnz = nnz)
+        self.interp = PETSc.Mat().create(comm=self.ensemble_comm.comm)
+        self.interp.setSizes(((self.n_mesh_local, -1), (self.n_data_local, -1)))
+        self.interp.setFromOptions()
         self.interp.setUp()
 
-        istart, iend = self.interp.getOwnershipRange()
+    def assemble(self):
+        "compute values and assemble interpolation matrix"
 
-        for i in range(istart, iend):
-            cell = function_space.mesh().locate_cell(coords[i])
-            nodes = function_space.cell_node_list[cell]
-            for j in nodes:
-                temp_array = np.zeros(self.n_mesh)
-                temp_array[j] = 1.
-                dummy_function = Function(function_space, val = temp_array)
-                value = dummy_function.at(coords[i])
-                if value > 0.:
-                    self.interp.setValue(i, j, value)
+        meshvals_local = np.copy(self.function_space.mesh().coordinates.vector().dat.data_with_halos)
+        imin, imax = self.interp.getOwnershipRange()
+
+        # loop over all data points
+
+        for i in range(self.n_data):
+            cell = self.function_space.mesh().locate_cell(self.coords[i])
+            if (not cell is None):
+                nodes = self.function_space.cell_node_list[cell]
+                points = meshvals_local[nodes]
+                interp_coords = interpolate(self.coords[i], points)
+                for (node, val) in zip(nodes, interp_coords):
+                    if node < self.n_mesh_local:
+                        self.interp.setValue(imin + node, i, val)
 
         self.interp.assemble()
+
+        self.interp.view()
+
+    def _gather(self):
+        "wrapper to transfer data from distributed dataspace vector to root"
+
+        self.petsc_scatter.scatter(self.dataspace_distrib, self.dataspace_gathered,
+                                   mode=PETSc.ScatterMode.SCATTER_FORWARD)
+
+    def _scatter(self):
+        "wrapper to transfer data from root to distributed dataspace vector"
+
+        self.petsc_scatter.scatter(self.dataspace_gathered, self.dataspace_distrib,
+                                   mode=PETSc.ScatterMode.SCATTER_REVERSE)
+
+    def destroy(self):
+        "deallocate memory for PETSc vectors and matrix"
+
+        self.interp.destroy()
+        self.dataspace_gathered.destroy()
+        self.dataspace_distrib.destroy()
+
+    def interp_data_to_mesh(self, data_array):
+        "take a gathered numpy array in the data space and interpolate to a distributed mesh vector"
+
+        data_array = np.array(data_array)
+
+        assert data_array.ndim == 1
+        assert data_array.shape[0] == self.dataspace_gathered.getSizes()[0], "data_array has bad shape"
+
+        # scatter into dataspace_distrib
+
+        self.dataspace_gathered.array = np.copy(data_array)
+        self._scatter()
+
+        with self.meshspace_vector.dat.vec as vec:
+            self.interp.mult(self.dataspace_distrib, vec)
+
+        return self.meshspace_vector.copy()
+
+    def interp_mesh_to_data(self, input_mesh_vector):
+        "take a distributed mesh vector and interpolate to a gathered numpy array"
+
+        # check vector local sizes and copy values
+        assert (input_mesh_vector.local_size() == self.meshspace_vector.local_size() and
+                input_mesh_vector.size() == self.meshspace_vector.size()), "bad size for input vector"
+        self.meshspace_vector.set_local(input_mesh_vector.get_local())
+
+        # interpolate to dataspace and gather, returning numpy array
+
+        with self.meshspace_vector.dat.vec_ro as b:
+            self.interp.multTranspose(b, self.dataspace_distrib)
+
+        self._gather()
+
+        return np.copy(self.dataspace_gathered.array)
+
+    def get_meshspace_column_vector(self, idx):
+        "returns distributed meshspace column vector for a given data point"
+
+        assert idx >= 0 and idx < self.n_data, "idx out of range"
+
+        f = Function(self.function_space).vector()
+
+        f.set_local(self.interp.getColumnVector(idx).array)
+
+        return f
+
+    def __str__(self):
+        "return string representation of interpolation matrix"
+
+        return "Interpolation matrix from %d mesh points to %d data points".format(self.n_mesh, self.n_data)
+
+def interpolate(data_coord, nodal_points):
+    """
+    interpolate between nodal points and data point
+    note at present this only works for triangular/tetrahedral meshes
+    """
+
+    if nodal_points.ndim == 1:
+        nodal_points = np.reshape(nodal_points, (-1, 1))
+    ndim = len(data_coord)
+    assert nodal_points.shape == (ndim + 1, ndim)
+
+    A = np.ones((ndim + 1, ndim + 1))
+    A[0:-1,:] = np.transpose(nodal_points)
+    b = np.array(list(data_coord) + [1.])
+
+    val = np.linalg.solve(A, b)
+
+    eps = 1.e-10
+
+    assert np.abs(np.sum(val) - 1.) <= eps, "interpolation between data and coordinates failed"
+    assert np.all(val > -eps), "interpolation routine failed, point is not in the given cell"
+
+    return val
--- stat_fem/tests/test_InterpolationMatrix.py
+++ stat_fem/tests/test_InterpolationMatrix.py
@@ -1,38 +1,48 @@
 import numpy as np
 from numpy.testing import assert_allclose
 import pytest
+from firedrake import Ensemble, COMM_WORLD
 from firedrake.utility_meshes import UnitIntervalMesh
 from firedrake.functionspace import FunctionSpace
-from ..InterpolationMatrix import InterpolationMatrix
-from firedrake.function import PointNotInDomainError
+from ..InterpolationMatrix import InterpolationMatrix, interpolate
+from firedrake.function import Function
 
 def test_InterpolationMatrix():
-    "test InterpolationMatrix"
+    "test InterpolationMatrix with multiple processes"
 
-    nx = 2
+    n_proc = COMM_WORLD.size
 
-    coords = np.array([[0.5]])
+    nx = 7
+
+    coords = np.array([[0.75], [0.5], [0.25], [0.1]])
+    nd = len(coords)
 
     mesh = UnitIntervalMesh(nx)
     V = FunctionSpace(mesh, "CG", 1)
+    vec = Function(V).vector()
+
+    ens = Ensemble(COMM_WORLD, n_proc)
 
     im = InterpolationMatrix(coords, V)
 
-    interp_expected = np.array([[0., 1., 0.]])
+    if COMM_WORLD.rank == 0:
+        gathered_sizes = (nd, nd)
+    else:
+        gathered_sizes = (0, 0)
 
-    for i in range(1):
-        for j in range(nx + 1):
-            assert_allclose(im.interp.getValue(i, j), interp_expected[i, j], atol = 1.e-8, rtol = 1.e-6)
-
-    coords = np.array([[0.25]])
-
-    im = InterpolationMatrix(coords, V)
-
-    interp_expected = np.array([[0.5, 0.5, 0.]])
-
-    for i in range(1):
-        for j in range(nx + 1):
-            assert_allclose(im.interp.getValue(i, j), interp_expected[i, j], atol = 1.e-8, rtol = 1.e-6)
+    assert im.n_data == nd
+    assert im.n_data_local == nd//n_proc
+    assert im.n_mesh == vec.size()
+    assert im.n_mesh_local == vec.local_size()
+    assert_allclose(im.coords, coords)
+    assert im.function_space == V
+    assert im.ensemble_comm.ensemble_comm.size == ens.ensemble_comm.size
+    assert im.ensemble_comm.comm.size == ens.comm.size
+    assert im.meshspace_vector.size() == vec.size()
+    assert im.meshspace_vector.local_size() == vec.local_size()
+    assert im.dataspace_distrib.getSizes() == (nd//n_proc, nd)
+    assert im.dataspace_gathered.getSizes() == gathered_sizes
+    assert im.interp.getSizes() == ((vec.local_size(), vec.size()), (nd//n_proc, nd))
 
 def test_InterpolationMatrix_failures():
     "test situation where InterpolationMatrix should fail"
@@ -44,21 +54,296 @@ def test_InterpolationMatrix_failures():
     with pytest.raises(TypeError):
         InterpolationMatrix(coords, 1.)
 
-    # coords out of domain
+    # bad shape for coords
 
-    coords = np.array([[-1.]])
+    coords = np.array([[0.5, 0.5]])
 
     nx = 2
 
     mesh = UnitIntervalMesh(nx)
     V = FunctionSpace(mesh, "CG", 1)
 
-    with pytest.raises(PointNotInDomainError):
-        InterpolationMatrix(coords, V)
-
-    # bad shape for coords
-
-    coords = np.array([[0.5, 0.5]])
-
     with pytest.raises(AssertionError):
         InterpolationMatrix(coords, V)
+
+    # bad argument for ensemble comm
+
+    coords = np.array([[0.5]])
+
+    with pytest.raises(TypeError):
+        InterpolationMatrix(coords, V, 1)
+
+def test_InterpolationMatrix_assemble():
+    "test the assemble method of interpolation matrix"
+
+    # simple 1D test
+
+    nx = 10
+
+    coords = np.array([[0.75], [0.5], [0.25], [0.125]])
+    nd = len(coords)
+
+    mesh = UnitIntervalMesh(nx)
+    V = FunctionSpace(mesh, "CG", 1)
+
+    im = InterpolationMatrix(coords, V)
+    im.assemble()
+
+    meshcoords = V.mesh().coordinates.vector().gather()
+    meshcoords_ordered = np.linspace(0., 1., nx + 1)
+
+    interp_ordered = np.transpose(np.array([[0., 0., 0., 0., 0., 0., 0., 0.5, 0.5, 0., 0.],
+                                            [0., 0., 0., 0., 0., 1., 0., 0., 0., 0., 0.],
+                                            [0., 0., 0.5, 0.5, 0., 0., 0., 0., 0., 0., 0.],
+                                            [0., 0.75, 0.25, 0., 0., 0., 0., 0., 0., 0., 0.]]))
+    interp_expected = np.zeros((nx + 1, nd))
+
+    for i in range(nx + 1):
+        interp_expected[np.where(meshcoords == meshcoords_ordered[i]),:] = interp_ordered[i,:]
+
+    imin, imax = im.interp.getOwnershipRange()
+
+    for i in range(imin, imax):
+        for j in range(nd):
+            assert_allclose(im.interp.getValue(i, j), interp_expected[i,j])
+
+    im.destroy()
+
+def test_InterpolationMatrix_gather():
+    "test the method to gather data at root process"
+
+    nx = 10
+
+    coords = np.array([[0.75], [0.5], [0.25], [0.125]])
+    nd = len(coords)
+
+    mesh = UnitIntervalMesh(nx)
+    V = FunctionSpace(mesh, "CG", 1)
+
+    im = InterpolationMatrix(coords, V)
+
+    im.dataspace_distrib.set(5.)
+
+    im._gather()
+
+    assert_allclose(im.dataspace_gathered.array, 5.)
+
+def test_InterpolationMatrix_scatter():
+    "test the method to scatter data to distributed vectors"
+
+    nx = 10
+
+    coords = np.array([[0.75], [0.5], [0.25], [0.125]])
+    nd = len(coords)
+
+    mesh = UnitIntervalMesh(nx)
+    V = FunctionSpace(mesh, "CG", 1)
+
+    im = InterpolationMatrix(coords, V)
+
+    im.dataspace_gathered.set(5.)
+
+    im._scatter()
+
+    assert_allclose(im.dataspace_distrib.array, 5.)
+
+def test_InterpolationMatrix_interp_data_to_mesh():
+    "test method to interpolate data to the mesh"
+
+    # simple 1D test
+
+    nx = 10
+
+    coords = np.array([[0.75], [0.5], [0.25], [0.125]])
+    nd = len(coords)
+
+    mesh = UnitIntervalMesh(nx)
+    V = FunctionSpace(mesh, "CG", 1)
+
+    im = InterpolationMatrix(coords, V)
+    im.assemble()
+
+    if COMM_WORLD.rank == 0:
+        data = np.array([2., 1., 3., 3.])
+    else:
+        data = np.zeros(0)
+
+    expected_ordered = np.array([0.  , 2.25, 2.25, 1.5 , 0.  , 1.  , 0.  , 1.  , 1.  , 0.  , 0.  ])
+    expected = np.zeros(nx + 1)
+
+    meshcoords = V.mesh().coordinates.vector().gather()
+    meshcoords_ordered = np.linspace(0., 1., nx + 1)
+
+    for i in range(nx + 1):
+        expected[np.where(meshcoords == meshcoords_ordered[i])] = expected_ordered[i]
+
+    out = im.interp_data_to_mesh(data)
+
+    with out.dat.vec_ro as vec:
+        imin, imax = im.interp.getOwnershipRange()
+        for i in range(imin, imax):
+            assert_allclose(vec.getValue(i), expected[i])
+
+    # bad input size and shape
+
+    if COMM_WORLD.rank == 0:
+        in_vec = np.zeros(nd + 1)
+    else:
+        in_vec = np.zeros(1)
+
+    with pytest.raises(AssertionError):
+        im.interp_data_to_mesh(in_vec)
+
+    if COMM_WORLD.rank == 0:
+        in_vec = np.zeros((nd + 1, 1))
+    else:
+        in_vec = np.zeros((1, 1))
+
+    with pytest.raises(AssertionError):
+        im.interp_data_to_mesh(in_vec)
+
+def test_InterpolationMatrix_interp_mesh_to_data():
+    "test method to interpolate from distributed mesh to data gathered at root"
+
+    # simple 1D test
+
+    nx = 10
+
+    coords = np.array([[0.75], [0.5], [0.25], [0.125]])
+    nd = len(coords)
+
+    mesh = UnitIntervalMesh(nx)
+    V = FunctionSpace(mesh, "CG", 1)
+
+    im = InterpolationMatrix(coords, V)
+    im.assemble()
+
+    input_ordered = np.array([3., 2., 7., 4., 0., 0., 2., 1., 1., 1., 5.])
+
+    f = Function(V).vector()
+
+    meshcoords = V.mesh().coordinates.vector().gather()
+    meshcoords_ordered = np.linspace(0., 1., nx + 1)
+
+    with f.dat.vec as vec:
+        imin, imax = vec.getOwnershipRange()
+        for i in range(imin, imax):
+            vec.setValue(i, input_ordered[np.where(meshcoords_ordered == meshcoords[i])])
+
+    if COMM_WORLD.rank == 0:
+        expected = np.array([1.  , 0.  , 5.5 , 3.25])
+    else:
+        expected = np.zeros(0)
+
+    out = im.interp_mesh_to_data(f)
+
+    assert_allclose(out, expected)
+
+    # failure due to bad input sizes
+
+    mesh2 = UnitIntervalMesh(nx + 1)
+    V2 = FunctionSpace(mesh2, "CG", 1)
+
+    f2 = Function(V2).vector()
+    f2.set_local(np.ones(f2.local_size()))
+
+    with pytest.raises(AssertionError):
+        im.interp_mesh_to_data(f2)
+
+    im.destroy()
+
+def test_InterpolationMatrix_get_meshspace_column_vector():
+    "test the get_meshspace_column_vector method"
+
+    nx = 10
+
+    coords = np.array([[0.75], [0.5], [0.25], [0.125]])
+    nd = len(coords)
+
+    mesh = UnitIntervalMesh(nx)
+    V = FunctionSpace(mesh, "CG", 1)
+
+    im = InterpolationMatrix(coords, V)
+    im.assemble()
+
+    vec_expected_ordered = np.array([0., 0., 0., 0., 0., 0., 0., 0.5, 0.5, 0., 0.])
+    vec_expected = np.zeros(nx + 1)
+
+    meshcoords = V.mesh().coordinates.vector().gather()
+    meshcoords_ordered = np.linspace(0., 1., nx + 1)
+
+    for i in range(nx + 1):
+        vec_expected[np.where(meshcoords == meshcoords_ordered[i])] = vec_expected_ordered[i]
+
+    vec = im.get_meshspace_column_vector(0)
+
+    with vec.dat.vec_ro as petsc_vec:
+        imin, imax = petsc_vec.getOwnershipRange()
+        for i in range(imin, imax):
+            assert_allclose(petsc_vec.getValue(i), vec_expected[i])
+
+    # index out of range
+
+    with pytest.raises(AssertionError):
+        im.get_meshspace_column_vector(5)
+
+def test_InterpolationMatrix_str():
+    "test the str method of InterpolationMatrix"
+
+    nx = 10
+
+    coords = np.array([[0.75], [0.5], [0.25], [0.1]])
+    nd = len(coords)
+
+    mesh = UnitIntervalMesh(nx)
+    V = FunctionSpace(mesh, "CG", 1)
+
+    im = InterpolationMatrix(coords, V)
+
+    assert str(im) == "Interpolation matrix from %d mesh points to %d data points".format(nx + 1, nd)
+
+def test_interpolate():
+    "test the interpolate function"
+
+    # 1D
+
+    data_coord = np.array([0.5])
+    nodal_points = np.array([[0.], [1.]])
+
+    val = interpolate(data_coord, nodal_points)
+
+    assert_allclose(val, [0.5, 0.5])
+
+    # 2D
+
+    data_coord = np.array([0.5, 0.5])
+    nodal_points = np.array([[0., 0.], [1., 0.], [0., 1.]])
+
+    val = interpolate(data_coord, nodal_points)
+
+    assert_allclose(val, [0., 0.5, 0.5])
+
+    # 3D
+
+    data_coord = np.array([1./3., 1./3., 1./3.])
+    nodal_points = np.array([[0., 0., 0.], [1., 0., 0.], [0., 1., 0.], [0., 0., 1.]])
+
+    val = interpolate(data_coord, nodal_points)
+
+    assert_allclose(val, [0., 1./3., 1./3., 1./3.], atol = 1.e-9)
+
+    # failure: bad dimensions
+
+    data_coord = np.array([0.5, 0.5])
+    nodal_points = np.array([[0., 0., 0.], [1., 0., 0.], [0., 1., 0.], [0., 0., 1.]])
+
+    with pytest.raises(AssertionError):
+        interpolate(data_coord, nodal_points)
+
+    # failure: point not in domain
+
+    data_coord = np.array([0.5, 0.5])
+    nodal_points = np.array([[0., 0.], [0.1, 0.], [0., 0.1]])
+
+    with pytest.raises(AssertionError):
+        interpolate(data_coord, nodal_points)
