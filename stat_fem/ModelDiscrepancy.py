89d6704b93459fca9038d33b8641337aa183fb99
edaub <edaub@turing.ac.uk>
2019-12-02 15:17:43 +0000

    added model discrepancy constant factor to class

--- stat_fem/ModelDiscrepancy.py
+++ stat_fem/ModelDiscrepancy.py
@@ -4,16 +4,23 @@ from .ObsData import ObsData
 
 class ModelDiscrepancy(object):
     "class representing model discrepancy for a model and observations"
-    def __init__(self, data_obs, sigma, l, cov=sqexp):
+    def __init__(self, data_obs, rho, sigma, l, cov=sqexp):
         "create a new model discrepancy object given data and a covariance function"
 
         assert isinstance(data_obs, ObsData), "data_obs must be of type ObsData"
+        assert rho > 0., "scaling factor must be positive"
         assert sigma > 0., "covariance scale must be positive"
         assert l > 0., "covariance length scale must be positive"
 
+        self.rho = float(rho)
         self.K = cov(data_obs.get_coords(), data_obs.get_coords(), sigma, l)
         self.sigma_dat = data_obs.get_unc()
         self.n_obs = data_obs.n_obs
+
+    def get_rho(self):
+        "returns scaling factor"
+
+        return self.rho
 
     def get_K(self):
         "returns model discrepancy covariance matrix"
--- stat_fem/tests/test_ModelDiscrepancy.py
+++ stat_fem/tests/test_ModelDiscrepancy.py
@@ -11,14 +11,16 @@ def test_ModelDiscrepancy_init():
     data = np.array([1., 2., 3.])
     unc = 0.1
 
+    rho = 1.
     sigma = 1.
     l = 1.
 
     od = ObsData(coords, data, unc)
 
-    md = ModelDiscrepancy(od, sigma, l)
+    md = ModelDiscrepancy(od, rho, sigma, l)
 
     assert md.n_obs == 3
+    assert_allclose(md.rho, rho)
     assert_allclose(md.sigma_dat, unc)
 
     r = np.array([[0., np.sqrt(5.), 1.], [np.sqrt(5.), 0., np.sqrt(2.)], [1., np.sqrt(2.), 0.]])
@@ -33,12 +35,13 @@ def test_ModelDiscrepancy_get_K():
     data = np.array([1., 2., 3.])
     unc = 0.1
 
+    rho = 1.
     sigma = 1.
     l = 1.
 
     od = ObsData(coords, data, unc)
 
-    md = ModelDiscrepancy(od, sigma, l)
+    md = ModelDiscrepancy(od, rho, sigma, l)
 
     r = np.array([[0., np.sqrt(5.), 1.], [np.sqrt(5.), 0., np.sqrt(2.)], [1., np.sqrt(2.), 0.]])
     K = sigma**2*np.exp(-0.5*r**2/l**2)
@@ -52,14 +55,32 @@ def test_ModelDiscrepancy_get_K_plus_sigma():
     data = np.array([1., 2., 3.])
     unc = 0.1
 
+    rho = 1.
     sigma = 1.
     l = 1.
 
     od = ObsData(coords, data, unc)
 
-    md = ModelDiscrepancy(od, sigma, l)
+    md = ModelDiscrepancy(od, rho, sigma, l)
 
     r = np.array([[0., np.sqrt(5.), 1.], [np.sqrt(5.), 0., np.sqrt(2.)], [1., np.sqrt(2.), 0.]])
     K = sigma**2*np.exp(-0.5*r**2/l**2) + np.eye(3)*unc**2
 
     assert_allclose(md.get_K_plus_sigma(), K)
+
+def test_ModelDiscrepancy_get_rho():
+    "test the get_rho method of ModelDiscrepancy"
+
+    coords = np.array([[2., 1.], [0., 2.], [1., 1.]])
+    data = np.array([1., 2., 3.])
+    unc = 0.1
+
+    rho = 1.
+    sigma = 1.
+    l = 1.
+
+    od = ObsData(coords, data, unc)
+
+    md = ModelDiscrepancy(od, rho, sigma, l)
+
+    assert_allclose(md.get_rho(), rho)
