fixed bugs in forcing covariance implementaiton

stat_fem/ForcingCovariance.py
@@ -55,8 +55,12 @@ class ForcingCovariance(object):
 
         G = PETSc.Mat().create(comm=self.comm)
         G.setSizes(((self.nx_local, -1), (self.nx_local, -1)))
+        G.setFromOptions()
+        G.setUp()
 
         self.local_startind, self.local_endind = G.getOwnershipRange()
+
+        G.destroy()
 
     def _integrate_basis_functions(self):
         "integrate the basis functions for computing the forcing covariance"
@@ -76,17 +80,19 @@ class ForcingCovariance(object):
         mesh = self.function_space.ufl_domain()
         W = VectorFunctionSpace(mesh, self.function_space.ufl_element())
         X = interpolate(mesh.coordinates, W)
-        meshvals = np.array(X.dat.data_ro)
+        meshvals = X.vector().gather()
 
         for i in range(self.local_startind, self.local_endind):
             diag = (int_basis[i]*int_basis[i]*
-                    self.cov(meshvals[i], meshvals[i], self.sigma,self.l))
-            G_dict[(i, i)] = diag*(1.+self.regularization)
+                    self.cov(meshvals[i], meshvals[i], self.sigma, self.l)[0,0])
+            G_dict[(i, i)] = diag+self.regularization
             current_nnz += 1
-            for j in range(i+1, self.nx):
+            for j in range(0, self.nx):
+                if j == i:
+                    continue
                 new_element = (int_basis[i]*int_basis[j]*
                                self.cov(meshvals[i], meshvals[j],
-                                        self.sigma, self.l))
+                                        self.sigma, self.l)[0,0])
                 if new_element/diag > self.cutoff:
                     G_dict[(i, j)] = new_element
                     current_nnz += 1
@@ -95,25 +101,21 @@ class ForcingCovariance(object):
 
         return G_dict, nnz
 
-    def _generate_G(self):
+    def assemble(self):
         "compute values of G and create sparse matrix"
 
         G_dict, nnz = self._compute_G_vals()
 
         self.G = PETSc.Mat().create(comm=self.comm)
-        self.G.setType('sbaij')
+        self.G.setType('aij')
         self.G.setSizes(((self.nx_local, -1), (self.nx_local, -1)))
-        self.G.setPreallocationNNZ(nnz)
+        self.G.setPreallocationNNZ(max(nnz))
         self.G.setFromOptions()
         self.G.setUp()
 
         for key, val in G_dict.items():
             self.G.setValue(key[0], key[1], val)
 
-    def assemble(self):
-        "compute values of covariance forcing and assemble the sparse matrix"
-
-        self._generate_G()
         self.G.assemble()
 
     def destroy(self):

stat_fem/tests/test_ForcingCovariance.py
@@ -5,7 +5,8 @@ from firedrake import COMM_WORLD
 from firedrake.ensemble import Ensemble
 from firedrake.function import Function
 from firedrake.utility_meshes import UnitIntervalMesh
-from firedrake.functionspace import FunctionSpace
+from firedrake.functionspace import FunctionSpace, VectorFunctionSpace
+from firedrake.interpolation import interpolate
 from firedrake.petsc import PETSc
 import pytest
 from ..ForcingCovariance import ForcingCovariance
@@ -32,6 +33,8 @@ def test_ForcingCovariance_init():
 
     M = PETSc.Mat().create()
     M.setSizes(((n_local, -1), (n_local, -1)))
+    M.setFromOptions()
+    M.setUp()
     start, end = M.getOwnershipRange()
 
     assert fc.nx == n
@@ -123,65 +126,27 @@ def test_ForcingCovariance_compute_G_vals():
     cutoff = 0.
     regularization = 1.e-8
 
-    basis_ordered = np.array([0.05, 0.1 , 0.1 , 0.1 , 0.1 , 0.1 , 0.1 , 0.1 , 0.1 , 0.1 , 0.05])
-    basis = np.zeros(nx + 1)
-
-    meshcoords = V.mesh().coordinates.vector().gather()
-    meshcoords_ordered = np.linspace(0., 1., nx + 1)
-
-    for i in range(nx + 1):
-        basis[np.where(meshcoords == meshcoords_ordered[i])] = basis_ordered[i]
+    W = VectorFunctionSpace(mesh, V.ufl_element())
+    X = interpolate(mesh.coordinates, W)
+    meshcoords = X.vector().gather()
 
     fc = ForcingCovariance(V, sigma, l, cutoff, regularization)
 
-    r = cdist(np.atleast_2d(meshcoords), np.atleast_2d(meshcoords))
+    basis = fc._integrate_basis_functions()
+
+    r = cdist(np.reshape(meshcoords, (-1, 1)), np.reshape(meshcoords, (-1, 1)))
     cov_expected = (np.outer(basis, basis)*sigma**2*np.exp(-0.5*r**2/l**2) +
                     np.eye(nx + 1)*regularization)
-    nnz_expected = list(range(11, 0, -1))[fc.local_startind:fc.local_endind]
+    nnz_expected = [nx + 1]*(fc.local_endind - fc.local_startind)
 
     G_dict, nnz = fc._compute_G_vals()
 
     assert nnz == nnz_expected
 
-    for i in range(fc.local_startind, fc.local_endind):
-        for j in range(i, nx + 1):
-            assert_allclose(G_dict[(i,j)], cov_expected[i, j], atol = 1.e-8, rtol = 1.e-6)
+    for key, val in G_dict.items():
+        assert_allclose(val, cov_expected[key[0], key[1]], atol = 1.e-8, rtol = 1.e-6)
 
-def test_ForcingCovariance_generate_G():
-    "test the generate_G method of Forcing Covariance"
-
-    nx = 10
-
-    mesh = UnitIntervalMesh(nx)
-    V = FunctionSpace(mesh, "CG", 1)
-    sigma = 1.
-    l = 0.1
-    cutoff = 0.
-    regularization = 1.e-8
-
-    basis_ordered = np.array([0.05, 0.1 , 0.1 , 0.1 , 0.1 , 0.1 , 0.1 , 0.1 , 0.1 , 0.1 , 0.05])
-    basis = np.zeros(nx + 1)
-
-    meshcoords = V.mesh().coordinates.vector().gather()
-    meshcoords_ordered = np.linspace(0., 1., nx + 1)
-
-    for i in range(nx + 1):
-        basis[np.where(meshcoords == meshcoords_ordered[i])] = basis_ordered[i]
-
-    fc = ForcingCovariance(V, sigma, l, cutoff, regularization)
-
-    r = cdist(np.atleast_2d(meshcoords), np.atleast_2d(meshcoords))
-    cov_expected = (np.outer(basis, basis)*sigma**2*np.exp(-0.5*r**2/l**2) +
-                    np.eye(nx + 1)*regularization)
-
-    fc._generate_G()
-    fc.G.assemble()
-
-    for i in range(fc.local_startind, fc.local_endind):
-        for j in range(i, nx + 1):
-            assert_allclose(fc.G.getValue(i, j), cov_expected[i, j], atol = 1.e-8, rtol = 1.e-6)
-
-    fc.destroy()
+    assert len(G_dict) == len(cov_expected[fc.local_startind:fc.local_endind,:].flatten())
 
 def test_ForcingCovariance_assemble():
     "test the generate_G method of Forcing Covariance"
@@ -195,25 +160,21 @@ def test_ForcingCovariance_assemble():
     cutoff = 0.
     regularization = 1.e-8
 
-    basis_ordered = np.array([0.05, 0.1 , 0.1 , 0.1 , 0.1 , 0.1 , 0.1 , 0.1 , 0.1 , 0.1 , 0.05])
-    basis = np.zeros(nx + 1)
-
-    meshcoords = V.mesh().coordinates.vector().gather()
-    meshcoords_ordered = np.linspace(0., 1., nx + 1)
-
-    for i in range(nx + 1):
-        basis[np.where(meshcoords == meshcoords_ordered[i])] = basis_ordered[i]
+    W = VectorFunctionSpace(mesh, V.ufl_element())
+    X = interpolate(mesh.coordinates, W)
+    meshcoords = X.vector().gather()
 
     fc = ForcingCovariance(V, sigma, l, cutoff, regularization)
+    basis = fc._integrate_basis_functions()
 
-    r = cdist(np.atleast_2d(meshcoords), np.atleast_2d(meshcoords))
+    r = cdist(np.reshape(meshcoords, (-1, 1)), np.reshape(meshcoords, (-1, 1)))
     cov_expected = (np.outer(basis, basis)*sigma**2*np.exp(-0.5*r**2/l**2) +
                     np.eye(nx + 1)*regularization)
 
     fc.assemble()
 
     for i in range(fc.local_startind, fc.local_endind):
-        for j in range(i, nx + 1):
+        for j in range(0, nx + 1):
             assert_allclose(fc.G.getValue(i, j), cov_expected[i, j], atol = 1.e-8, rtol = 1.e-6)
 
     fc.destroy()
@@ -252,7 +213,6 @@ def test_ForcingCovariance_get_nx_local():
 
     assert fc.get_nx_local() == n_local
 
-@pytest.mark.mpi_skip
 def test_ForcingCovariance_str():
     "test the string method of ForcingCovariance"
 