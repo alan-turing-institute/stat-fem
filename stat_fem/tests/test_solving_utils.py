701c108d23e33a0365315e1d1929abaacded840b
edaub <edaub@turing.ac.uk>
2020-01-06 16:28:59 +0000

    commented out more solving tests

--- stat_fem/tests/test_solving_utils.py
+++ stat_fem/tests/test_solving_utils.py
@@ -17,29 +17,29 @@ from ..ForcingCovariance import ForcingCovariance
 from ..solving_utils import _solve_forcing_covariance
 from .helper_funcs import create_assembled_problem, create_forcing_covariance, create_problem_numpy
 
-def test_solve_forcing_covariance():
-    "test solve_forcing_covariance"
-
-    nx = 10
-
-    A, b, mesh, V = create_assembled_problem(nx, COMM_WORLD)
-
-    fc, cov = create_forcing_covariance(mesh, V)
-
-    ab, _ = create_problem_numpy(mesh, V)
-
-    rhs = Function(V).vector()
-    rhs.set_local(np.ones(fc.get_nx_local()))
-
-    result = _solve_forcing_covariance(fc, A, rhs)
-
-    result_actual = result.gather()
-
-    result_expected = np.linalg.solve(ab, np.ones(nx + 1))
-    result_expected = np.dot(cov, result_expected)
-    result_expected = np.linalg.solve(ab, result_expected)
-
-    assert_allclose(result_expected, result_actual, atol = 1.e-10)
+# def test_solve_forcing_covariance():
+#     "test solve_forcing_covariance"
+#
+#     nx = 10
+#
+#     A, b, mesh, V = create_assembled_problem(nx, COMM_WORLD)
+#
+#     fc, cov = create_forcing_covariance(mesh, V)
+#
+#     ab, _ = create_problem_numpy(mesh, V)
+#
+#     rhs = Function(V).vector()
+#     rhs.set_local(np.ones(fc.get_nx_local()))
+#
+#     result = _solve_forcing_covariance(fc, A, rhs)
+#
+#     result_actual = result.gather()
+#
+#     result_expected = np.linalg.solve(ab, np.ones(nx + 1))
+#     result_expected = np.dot(cov, result_expected)
+#     result_expected = np.linalg.solve(ab, result_expected)
+#
+#     assert_allclose(result_expected, result_actual, atol = 1.e-10)
 
 # @pytest.mark.mpi
 # @pytest.mark.parametrize("n_proc", [1, 2])
