0b406b339ed6caf2849e2c838d1f05afb85597a8
edaub <edaub@turing.ac.uk>
2020-01-07 18:56:39 +0000

    all unit tests back in

--- stat_fem/tests/test_CovarianceFunctions.py
+++ stat_fem/tests/test_CovarianceFunctions.py
@@ -1,276 +1,276 @@
-# import numpy as np
-# from numpy.testing import assert_allclose
-# import pytest
-# from ..CovarianceFunctions import sqexp, sqexp_deriv, sqexp_hessian, calc_r2
-#
-# def test_sqexp():
-#     "test the squared exponential covariance function"
-#
-#     # 1d input array
-#
-#     x = np.array([[1.], [2.], [3.]])
-#     sigma = 0.
-#     l = 0.
-#
-#     r = np.array([[0., 1., 2.], [1., 0., 1.], [2., 1., 0.]])
-#
-#     f_expected = np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)
-#     f_actual = sqexp(x, x, sigma, l)
-#
-#     assert_allclose(f_expected, f_actual, atol=1.e-10)
-#
-#     # 2d input array
-#
-#     x = np.array([[2., 1.], [0., 2.], [1., 1.]])
-#     sigma = 0.
-#     l = 0.
-#
-#     r = np.array([[0.,          np.sqrt(5.), 1.         ],
-#                   [np.sqrt(5.), 0.,          np.sqrt(2.)],
-#                   [1.,          np.sqrt(2.), 0.         ]])
-#
-#     f_expected = np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)
-#     f_actual = sqexp(x, x, sigma, l)
-#
-#     assert_allclose(f_expected, f_actual, atol=1.e-10)
-#
-#     # change parameters
-#
-#     x = np.array([[1.], [2.], [3.]])
-#     sigma = np.log(0.1)
-#     l = np.log(2.)
-#
-#     r = np.array([[0., 1., 2.], [1., 0., 1.], [2., 1., 0.]])
-#
-#     f_expected = np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)
-#     f_actual = sqexp(x, x, sigma, l)
-#
-#     assert_allclose(f_expected, f_actual, atol=1.e-10)
-#
-# def test_sqexp_deriv():
-#     "test derivative of squared exponential"
-#
-#     # 1d input array
-#
-#     x = np.array([[1.], [2.], [3.]])
-#     sigma = 0.
-#     l = 0.
-#
-#     r = np.array([[0., 1., 2.], [1., 0., 1.], [2., 1., 0.]])
-#
-#     f_expected = np.zeros((2, 3, 3))
-#     f_expected[0] = 2.*np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)
-#     f_expected[1] = np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)*r**2/np.exp(l)**2
-#     f_actual = sqexp_deriv(x, x, sigma, l)
-#
-#     assert_allclose(f_expected, f_actual, atol=1.e-10)
-#
-#     # 2d input array
-#
-#     x = np.array([[2., 1.], [0., 2.], [1., 1.]])
-#     sigma = 0.
-#     l = 0.
-#
-#     r = np.array([[0.,          np.sqrt(5.), 1.         ],
-#                   [np.sqrt(5.), 0.,          np.sqrt(2.)],
-#                   [1.,          np.sqrt(2.), 0.         ]])
-#
-#     f_expected = np.zeros((2, 3, 3))
-#     f_expected[0] = 2.*np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)
-#     f_expected[1] = np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)*r**2/np.exp(l)**2
-#     f_actual = sqexp_deriv(x, x, sigma, l)
-#
-#     assert_allclose(f_expected, f_actual, atol=1.e-10)
-#
-#     # change parameters
-#
-#     x = np.array([[1.], [2.], [3.]])
-#     sigma = np.log(0.1)
-#     l = np.log(2.)
-#
-#     r = np.array([[0., 1., 2.], [1., 0., 1.], [2., 1., 0.]])
-#
-#     f_expected = np.zeros((2, 3, 3))
-#     f_expected[0] = 2.*np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)
-#     f_expected[1] = np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)*r**2/np.exp(l)**2
-#     f_actual = sqexp_deriv(x, x, sigma, l)
-#
-#     assert_allclose(f_expected, f_actual, atol=1.e-10)
-#
-# def test_sqexp_deriv_fd():
-#     "test sqexp_deriv using finite differences"
-#
-#     dx = 1.e-6
-#
-#     # 1d input array
-#
-#     x = np.array([[1.], [2.], [3.]])
-#     sigma = 0.
-#     l = 0.
-#
-#     f_expected = np.zeros((2, 3, 3))
-#     f_expected[0] = (sqexp(x, x, sigma + dx, l) - sqexp(x, x, sigma, l))/dx
-#     f_expected[1] = (sqexp(x, x, sigma, l + dx) - sqexp(x, x, sigma, l))/dx
-#
-#     f_actual = sqexp_deriv(x, x, sigma, l)
-#
-#     assert_allclose(f_expected, f_actual, rtol=1.e-5, atol=1.e-10)
-#
-#     # 2d input array
-#
-#     x = np.array([[2., 1.], [0., 2.], [1., 1.]])
-#     sigma = 0.
-#     l = 0.
-#
-#     f_expected = np.zeros((2, 3, 3))
-#     f_expected[0] = (sqexp(x, x, sigma + dx, l) - sqexp(x, x, sigma, l))/dx
-#     f_expected[1] = (sqexp(x, x, sigma, l + dx) - sqexp(x, x, sigma, l))/dx
-#     f_actual = sqexp_deriv(x, x, sigma, l)
-#
-#     assert_allclose(f_expected, f_actual, rtol=1.e-5, atol=1.e-10)
-#
-#     # change parameters
-#
-#     x = np.array([[1.], [2.], [3.]])
-#     sigma = np.log(0.1)
-#     l = np.log(2.)
-#
-#     r = np.array([[0., 1., 2.], [1., 0., 1.], [2., 1., 0.]])
-#
-#     f_expected = np.zeros((2, 3, 3))
-#     f_expected[0] = (sqexp(x, x, sigma + dx, l) - sqexp(x, x, sigma, l))/dx
-#     f_expected[1] = (sqexp(x, x, sigma, l + dx) - sqexp(x, x, sigma, l))/dx
-#     f_actual = sqexp_deriv(x, x, sigma, l)
-#
-#     assert_allclose(f_expected, f_actual, rtol=1.e-5, atol=1.e-10)
-#
-# def test_sqexp_hessian():
-#     "test derivative of squared exponential"
-#
-#     # 1d input array
-#
-#     x = np.array([[1.], [2.], [3.]])
-#     sigma = 0.
-#     l = 0.
-#
-#     r = np.array([[0., 1., 2.], [1., 0., 1.], [2., 1., 0.]])
-#
-#     f_expected = np.zeros((2, 2, 3, 3))
-#     f_expected[0, 0] = 4.*np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)
-#     f_expected[0, 1] = 2.*np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)*r**2/np.exp(l)**2
-#     f_expected[1, 0] = f_expected[0, 1]
-#     f_expected[1, 1] = np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)*(r**4/np.exp(l)**4 -
-#                                                                         2.*r**2*np.exp(l)**2)
-#     f_actual = sqexp_hessian(x, x, sigma, l)
-#
-#     assert_allclose(f_expected, f_actual, atol=1.e-10)
-#
-#     # 2d input array
-#
-#     x = np.array([[2., 1.], [0., 2.], [1., 1.]])
-#     sigma = 0.
-#     l = 0.
-#
-#     r = np.array([[0.,          np.sqrt(5.), 1.         ],
-#                   [np.sqrt(5.), 0.,          np.sqrt(2.)],
-#                   [1.,          np.sqrt(2.), 0.         ]])
-#
-#     f_expected = np.zeros((2, 2, 3, 3))
-#     f_expected[0, 0] = 4.*np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)
-#     f_expected[0, 1] = 2.*np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)*r**2/np.exp(l)**2
-#     f_expected[1, 0] = f_expected[0, 1]
-#     f_expected[1, 1] = np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)*(r**4/np.exp(l)**4 -
-#                                                                         2.*r**2*np.exp(l)**2)
-#     f_actual = sqexp_hessian(x, x, sigma, l)
-#
-#     # change parameters
-#
-#     x = np.array([[1.], [2.], [3.]])
-#     sigma = np.log(0.1)
-#     l = np.log(2.)
-#
-#     r = np.array([[0., 1., 2.], [1., 0., 1.], [2., 1., 0.]])
-#
-#     f_expected = np.zeros((2, 2, 3, 3))
-#     f_expected[0, 0] = 4.*np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)
-#     f_expected[0, 1] = 2.*np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)*r**2/np.exp(l)**2
-#     f_expected[1, 0] = f_expected[0, 1]
-#     f_expected[1, 1] = np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)*(r**4/np.exp(l)**4 -
-#                                                                         2.*r**2*np.exp(l)**2)
-#     f_actual = sqexp_hessian(x, x, sigma, l)
-#
-# def test_sqexp_hessian_fd():
-#     "test the hessian function using finite differences"
-#
-#     dx = 1.e-6
-#
-#     # 1d input array
-#
-#     x = np.array([[1.], [2.], [3.]])
-#     sigma = 0.
-#     l = 0.
-#
-#     f_expected = np.zeros((2, 2, 3, 3))
-#     f_expected[0, 0] = (sqexp_deriv(x, x, sigma + dx, l)[0] - sqexp_deriv(x, x, sigma, l)[0])/dx
-#     f_expected[0, 1] = (sqexp_deriv(x, x, sigma, l + dx)[0] - sqexp_deriv(x, x, sigma, l)[0])/dx
-#     f_expected[1, 0] = (sqexp_deriv(x, x, sigma + dx, l)[1] - sqexp_deriv(x, x, sigma, l)[1])/dx
-#     f_expected[1, 1] = (sqexp_deriv(x, x, sigma, l + dx)[1] - sqexp_deriv(x, x, sigma, l)[1])/dx
-#
-#     f_actual = sqexp_hessian(x, x, sigma, l)
-#
-#     assert_allclose(f_expected, f_actual, rtol=1.e-5, atol=1.e-5)
-#
-#     # 2d input array
-#
-#     x = np.array([[2., 1.], [0., 2.], [1., 1.]])
-#     sigma = 0.
-#     l = 0.
-#
-#     f_expected = np.zeros((2, 2, 3, 3))
-#     f_expected[0, 0] = (sqexp_deriv(x, x, sigma + dx, l)[0] - sqexp_deriv(x, x, sigma, l)[0])/dx
-#     f_expected[0, 1] = (sqexp_deriv(x, x, sigma, l + dx)[0] - sqexp_deriv(x, x, sigma, l)[0])/dx
-#     f_expected[1, 0] = (sqexp_deriv(x, x, sigma + dx, l)[1] - sqexp_deriv(x, x, sigma, l)[1])/dx
-#     f_expected[1, 1] = (sqexp_deriv(x, x, sigma, l + dx)[1] - sqexp_deriv(x, x, sigma, l)[1])/dx
-#
-#     f_actual = sqexp_hessian(x, x, sigma, l)
-#
-#     assert_allclose(f_expected, f_actual, rtol=1.e-5, atol=1.e-5)
-#
-#     # change parameters
-#
-#     x = np.array([[1.], [2.], [3.]])
-#     sigma = np.log(0.1)
-#     l = np.log(2.)
-#
-#     f_expected = np.zeros((2, 2, 3, 3))
-#     f_expected[0, 0] = (sqexp_deriv(x, x, sigma + dx, l)[0] - sqexp_deriv(x, x, sigma, l)[0])/dx
-#     f_expected[0, 1] = (sqexp_deriv(x, x, sigma, l + dx)[0] - sqexp_deriv(x, x, sigma, l)[0])/dx
-#     f_expected[1, 0] = (sqexp_deriv(x, x, sigma + dx, l)[1] - sqexp_deriv(x, x, sigma, l)[1])/dx
-#     f_expected[1, 1] = (sqexp_deriv(x, x, sigma, l + dx)[1] - sqexp_deriv(x, x, sigma, l)[1])/dx
-#
-#     f_actual = sqexp_hessian(x, x, sigma, l)
-#
-#     assert_allclose(f_expected, f_actual, rtol=1.e-5, atol=1.e-5)
-#
-# def test_calc_r2():
-#     "test the method to calculate r2"
-#
-#     # 1d input array
-#
-#     x = np.array([[1.], [2.], [3.]])
-#
-#     r_expected = np.array([[0., 1., 4.], [1., 0., 1.], [4., 1., 0.]])
-#     r_actual = calc_r2(x, x)
-#
-#     assert_allclose(r_expected, r_actual)
-#
-#     # 2d input array
-#
-#     x = np.array([[2., 1.], [0., 2.], [1., 1.]])
-#
-#     r_expected = np.array([[0., 5., 1.],
-#                   [5., 0., 2.],
-#                   [1., 2., 0.]])
-#     r_actual = calc_r2(x, x)
-#
-#     assert_allclose(r_expected, r_actual)
+import numpy as np
+from numpy.testing import assert_allclose
+import pytest
+from ..CovarianceFunctions import sqexp, sqexp_deriv, sqexp_hessian, calc_r2
+
+def test_sqexp():
+    "test the squared exponential covariance function"
+
+    # 1d input array
+
+    x = np.array([[1.], [2.], [3.]])
+    sigma = 0.
+    l = 0.
+
+    r = np.array([[0., 1., 2.], [1., 0., 1.], [2., 1., 0.]])
+
+    f_expected = np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)
+    f_actual = sqexp(x, x, sigma, l)
+
+    assert_allclose(f_expected, f_actual, atol=1.e-10)
+
+    # 2d input array
+
+    x = np.array([[2., 1.], [0., 2.], [1., 1.]])
+    sigma = 0.
+    l = 0.
+
+    r = np.array([[0.,          np.sqrt(5.), 1.         ],
+                  [np.sqrt(5.), 0.,          np.sqrt(2.)],
+                  [1.,          np.sqrt(2.), 0.         ]])
+
+    f_expected = np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)
+    f_actual = sqexp(x, x, sigma, l)
+
+    assert_allclose(f_expected, f_actual, atol=1.e-10)
+
+    # change parameters
+
+    x = np.array([[1.], [2.], [3.]])
+    sigma = np.log(0.1)
+    l = np.log(2.)
+
+    r = np.array([[0., 1., 2.], [1., 0., 1.], [2., 1., 0.]])
+
+    f_expected = np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)
+    f_actual = sqexp(x, x, sigma, l)
+
+    assert_allclose(f_expected, f_actual, atol=1.e-10)
+
+def test_sqexp_deriv():
+    "test derivative of squared exponential"
+
+    # 1d input array
+
+    x = np.array([[1.], [2.], [3.]])
+    sigma = 0.
+    l = 0.
+
+    r = np.array([[0., 1., 2.], [1., 0., 1.], [2., 1., 0.]])
+
+    f_expected = np.zeros((2, 3, 3))
+    f_expected[0] = 2.*np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)
+    f_expected[1] = np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)*r**2/np.exp(l)**2
+    f_actual = sqexp_deriv(x, x, sigma, l)
+
+    assert_allclose(f_expected, f_actual, atol=1.e-10)
+
+    # 2d input array
+
+    x = np.array([[2., 1.], [0., 2.], [1., 1.]])
+    sigma = 0.
+    l = 0.
+
+    r = np.array([[0.,          np.sqrt(5.), 1.         ],
+                  [np.sqrt(5.), 0.,          np.sqrt(2.)],
+                  [1.,          np.sqrt(2.), 0.         ]])
+
+    f_expected = np.zeros((2, 3, 3))
+    f_expected[0] = 2.*np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)
+    f_expected[1] = np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)*r**2/np.exp(l)**2
+    f_actual = sqexp_deriv(x, x, sigma, l)
+
+    assert_allclose(f_expected, f_actual, atol=1.e-10)
+
+    # change parameters
+
+    x = np.array([[1.], [2.], [3.]])
+    sigma = np.log(0.1)
+    l = np.log(2.)
+
+    r = np.array([[0., 1., 2.], [1., 0., 1.], [2., 1., 0.]])
+
+    f_expected = np.zeros((2, 3, 3))
+    f_expected[0] = 2.*np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)
+    f_expected[1] = np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)*r**2/np.exp(l)**2
+    f_actual = sqexp_deriv(x, x, sigma, l)
+
+    assert_allclose(f_expected, f_actual, atol=1.e-10)
+
+def test_sqexp_deriv_fd():
+    "test sqexp_deriv using finite differences"
+
+    dx = 1.e-6
+
+    # 1d input array
+
+    x = np.array([[1.], [2.], [3.]])
+    sigma = 0.
+    l = 0.
+
+    f_expected = np.zeros((2, 3, 3))
+    f_expected[0] = (sqexp(x, x, sigma + dx, l) - sqexp(x, x, sigma, l))/dx
+    f_expected[1] = (sqexp(x, x, sigma, l + dx) - sqexp(x, x, sigma, l))/dx
+
+    f_actual = sqexp_deriv(x, x, sigma, l)
+
+    assert_allclose(f_expected, f_actual, rtol=1.e-5, atol=1.e-10)
+
+    # 2d input array
+
+    x = np.array([[2., 1.], [0., 2.], [1., 1.]])
+    sigma = 0.
+    l = 0.
+
+    f_expected = np.zeros((2, 3, 3))
+    f_expected[0] = (sqexp(x, x, sigma + dx, l) - sqexp(x, x, sigma, l))/dx
+    f_expected[1] = (sqexp(x, x, sigma, l + dx) - sqexp(x, x, sigma, l))/dx
+    f_actual = sqexp_deriv(x, x, sigma, l)
+
+    assert_allclose(f_expected, f_actual, rtol=1.e-5, atol=1.e-10)
+
+    # change parameters
+
+    x = np.array([[1.], [2.], [3.]])
+    sigma = np.log(0.1)
+    l = np.log(2.)
+
+    r = np.array([[0., 1., 2.], [1., 0., 1.], [2., 1., 0.]])
+
+    f_expected = np.zeros((2, 3, 3))
+    f_expected[0] = (sqexp(x, x, sigma + dx, l) - sqexp(x, x, sigma, l))/dx
+    f_expected[1] = (sqexp(x, x, sigma, l + dx) - sqexp(x, x, sigma, l))/dx
+    f_actual = sqexp_deriv(x, x, sigma, l)
+
+    assert_allclose(f_expected, f_actual, rtol=1.e-5, atol=1.e-10)
+
+def test_sqexp_hessian():
+    "test derivative of squared exponential"
+
+    # 1d input array
+
+    x = np.array([[1.], [2.], [3.]])
+    sigma = 0.
+    l = 0.
+
+    r = np.array([[0., 1., 2.], [1., 0., 1.], [2., 1., 0.]])
+
+    f_expected = np.zeros((2, 2, 3, 3))
+    f_expected[0, 0] = 4.*np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)
+    f_expected[0, 1] = 2.*np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)*r**2/np.exp(l)**2
+    f_expected[1, 0] = f_expected[0, 1]
+    f_expected[1, 1] = np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)*(r**4/np.exp(l)**4 -
+                                                                        2.*r**2*np.exp(l)**2)
+    f_actual = sqexp_hessian(x, x, sigma, l)
+
+    assert_allclose(f_expected, f_actual, atol=1.e-10)
+
+    # 2d input array
+
+    x = np.array([[2., 1.], [0., 2.], [1., 1.]])
+    sigma = 0.
+    l = 0.
+
+    r = np.array([[0.,          np.sqrt(5.), 1.         ],
+                  [np.sqrt(5.), 0.,          np.sqrt(2.)],
+                  [1.,          np.sqrt(2.), 0.         ]])
+
+    f_expected = np.zeros((2, 2, 3, 3))
+    f_expected[0, 0] = 4.*np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)
+    f_expected[0, 1] = 2.*np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)*r**2/np.exp(l)**2
+    f_expected[1, 0] = f_expected[0, 1]
+    f_expected[1, 1] = np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)*(r**4/np.exp(l)**4 -
+                                                                        2.*r**2*np.exp(l)**2)
+    f_actual = sqexp_hessian(x, x, sigma, l)
+
+    # change parameters
+
+    x = np.array([[1.], [2.], [3.]])
+    sigma = np.log(0.1)
+    l = np.log(2.)
+
+    r = np.array([[0., 1., 2.], [1., 0., 1.], [2., 1., 0.]])
+
+    f_expected = np.zeros((2, 2, 3, 3))
+    f_expected[0, 0] = 4.*np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)
+    f_expected[0, 1] = 2.*np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)*r**2/np.exp(l)**2
+    f_expected[1, 0] = f_expected[0, 1]
+    f_expected[1, 1] = np.exp(sigma)**2*np.exp(-0.5*r**2/np.exp(l)**2)*(r**4/np.exp(l)**4 -
+                                                                        2.*r**2*np.exp(l)**2)
+    f_actual = sqexp_hessian(x, x, sigma, l)
+
+def test_sqexp_hessian_fd():
+    "test the hessian function using finite differences"
+
+    dx = 1.e-6
+
+    # 1d input array
+
+    x = np.array([[1.], [2.], [3.]])
+    sigma = 0.
+    l = 0.
+
+    f_expected = np.zeros((2, 2, 3, 3))
+    f_expected[0, 0] = (sqexp_deriv(x, x, sigma + dx, l)[0] - sqexp_deriv(x, x, sigma, l)[0])/dx
+    f_expected[0, 1] = (sqexp_deriv(x, x, sigma, l + dx)[0] - sqexp_deriv(x, x, sigma, l)[0])/dx
+    f_expected[1, 0] = (sqexp_deriv(x, x, sigma + dx, l)[1] - sqexp_deriv(x, x, sigma, l)[1])/dx
+    f_expected[1, 1] = (sqexp_deriv(x, x, sigma, l + dx)[1] - sqexp_deriv(x, x, sigma, l)[1])/dx
+
+    f_actual = sqexp_hessian(x, x, sigma, l)
+
+    assert_allclose(f_expected, f_actual, rtol=1.e-5, atol=1.e-5)
+
+    # 2d input array
+
+    x = np.array([[2., 1.], [0., 2.], [1., 1.]])
+    sigma = 0.
+    l = 0.
+
+    f_expected = np.zeros((2, 2, 3, 3))
+    f_expected[0, 0] = (sqexp_deriv(x, x, sigma + dx, l)[0] - sqexp_deriv(x, x, sigma, l)[0])/dx
+    f_expected[0, 1] = (sqexp_deriv(x, x, sigma, l + dx)[0] - sqexp_deriv(x, x, sigma, l)[0])/dx
+    f_expected[1, 0] = (sqexp_deriv(x, x, sigma + dx, l)[1] - sqexp_deriv(x, x, sigma, l)[1])/dx
+    f_expected[1, 1] = (sqexp_deriv(x, x, sigma, l + dx)[1] - sqexp_deriv(x, x, sigma, l)[1])/dx
+
+    f_actual = sqexp_hessian(x, x, sigma, l)
+
+    assert_allclose(f_expected, f_actual, rtol=1.e-5, atol=1.e-5)
+
+    # change parameters
+
+    x = np.array([[1.], [2.], [3.]])
+    sigma = np.log(0.1)
+    l = np.log(2.)
+
+    f_expected = np.zeros((2, 2, 3, 3))
+    f_expected[0, 0] = (sqexp_deriv(x, x, sigma + dx, l)[0] - sqexp_deriv(x, x, sigma, l)[0])/dx
+    f_expected[0, 1] = (sqexp_deriv(x, x, sigma, l + dx)[0] - sqexp_deriv(x, x, sigma, l)[0])/dx
+    f_expected[1, 0] = (sqexp_deriv(x, x, sigma + dx, l)[1] - sqexp_deriv(x, x, sigma, l)[1])/dx
+    f_expected[1, 1] = (sqexp_deriv(x, x, sigma, l + dx)[1] - sqexp_deriv(x, x, sigma, l)[1])/dx
+
+    f_actual = sqexp_hessian(x, x, sigma, l)
+
+    assert_allclose(f_expected, f_actual, rtol=1.e-5, atol=1.e-5)
+
+def test_calc_r2():
+    "test the method to calculate r2"
+
+    # 1d input array
+
+    x = np.array([[1.], [2.], [3.]])
+
+    r_expected = np.array([[0., 1., 4.], [1., 0., 1.], [4., 1., 0.]])
+    r_actual = calc_r2(x, x)
+
+    assert_allclose(r_expected, r_actual)
+
+    # 2d input array
+
+    x = np.array([[2., 1.], [0., 2.], [1., 1.]])
+
+    r_expected = np.array([[0., 5., 1.],
+                  [5., 0., 2.],
+                  [1., 2., 0.]])
+    r_actual = calc_r2(x, x)
+
+    assert_allclose(r_expected, r_actual)
--- stat_fem/tests/test_assemble.py
+++ stat_fem/tests/test_assemble.py
@@ -1,87 +1,87 @@
-# import pytest
-# import numpy as np
-# from numpy.testing import assert_allclose
-# from ..assemble import assemble
-# from ..ForcingCovariance import ForcingCovariance
-# from ..InterpolationMatrix import InterpolationMatrix
-# from firedrake.matrix import Matrix
-# from firedrake import dx, TrialFunction, TestFunction, interpolate
-# from firedrake import dot, DirichletBC, grad, Function
-# from firedrake import COMM_WORLD, SpatialCoordinate, pi, sin
-# from .helper_funcs import create_interp, create_assembled_problem, create_forcing_covariance
-#
-# def test_assemble_ForcingCovariance():
-#     "test assemble with a forcing covariance object"
-#
-#     # assemble forcing covariance
-#
-#     nx = 10
-#
-#     _, _, mesh, V = create_assembled_problem(nx, COMM_WORLD)
-#     sigma = np.log(1.)
-#     l = np.log(0.1)
-#     cutoff = 0.
-#     regularization = 1.e-8
-#
-#     fc = ForcingCovariance(V, sigma, l, cutoff, regularization)
-#
-#     fc_2 = assemble(fc)
-#
-#     assert fc_2.is_assembled
-#
-#     _, cov_expected = create_forcing_covariance(mesh, V)
-#
-#     for i in range(fc_2.local_startind, fc_2.local_endind):
-#         for j in range(0, nx + 1):
-#             assert_allclose(fc_2.G.getValue(i, j), cov_expected[i, j], atol = 1.e-8, rtol = 1.e-6)
-#
-#     fc.destroy()
-#
-# def test_assemble_InterpolationMatrix():
-#     "test assemble with an interpolation matrix"
-#
-#     # assemble interpolation matrix
-#
-#     nx = 10
-#
-#     coords = np.array([[0.75], [0.5], [0.25], [0.125]])
-#     nd = len(coords)
-#
-#     A, b, mesh, V = create_assembled_problem(nx, COMM_WORLD)
-#
-#     im = InterpolationMatrix(V, coords)
-#
-#     im_2 = assemble(im)
-#
-#     assert im_2.is_assembled
-#
-#     interp_expected = create_interp(mesh, V)
-#
-#     imin, imax = im_2.interp.getOwnershipRange()
-#
-#     for i in range(imin, imax):
-#         for j in range(nd):
-#             assert_allclose(im_2.interp.getValue(i, j), interp_expected[i,j], atol = 1.e-10)
-#
-#     im.destroy()
-#
-# def test_assemble_firedrake():
-#     "test assemble with a firedrake object"
-#
-#     # assemble firedrake matrix
-#
-#     nx = 10
-#
-#     _, _, mesh, V = create_assembled_problem(nx, COMM_WORLD)
-#
-#     u = TrialFunction(V)
-#     v = TestFunction(V)
-#     f = Function(V)
-#     x = SpatialCoordinate(mesh)
-#     f.interpolate((4.*pi*pi)*sin(x[0]*pi*2))
-#     a = (dot(grad(v), grad(u))) * dx
-#     L = f * v * dx
-#     bc = DirichletBC(V, 0., "on_boundary")
-#     A = assemble(a, bcs = bc)
-#
-#     assert isinstance(A, Matrix)
+import pytest
+import numpy as np
+from numpy.testing import assert_allclose
+from ..assemble import assemble
+from ..ForcingCovariance import ForcingCovariance
+from ..InterpolationMatrix import InterpolationMatrix
+from firedrake.matrix import Matrix
+from firedrake import dx, TrialFunction, TestFunction, interpolate
+from firedrake import dot, DirichletBC, grad, Function
+from firedrake import COMM_WORLD, SpatialCoordinate, pi, sin
+from .helper_funcs import create_interp, create_assembled_problem, create_forcing_covariance
+
+def test_assemble_ForcingCovariance():
+    "test assemble with a forcing covariance object"
+
+    # assemble forcing covariance
+
+    nx = 10
+
+    _, _, mesh, V = create_assembled_problem(nx, COMM_WORLD)
+    sigma = np.log(1.)
+    l = np.log(0.1)
+    cutoff = 0.
+    regularization = 1.e-8
+
+    fc = ForcingCovariance(V, sigma, l, cutoff, regularization)
+
+    fc_2 = assemble(fc)
+
+    assert fc_2.is_assembled
+
+    _, cov_expected = create_forcing_covariance(mesh, V)
+
+    for i in range(fc_2.local_startind, fc_2.local_endind):
+        for j in range(0, nx + 1):
+            assert_allclose(fc_2.G.getValue(i, j), cov_expected[i, j], atol = 1.e-8, rtol = 1.e-6)
+
+    fc.destroy()
+
+def test_assemble_InterpolationMatrix():
+    "test assemble with an interpolation matrix"
+
+    # assemble interpolation matrix
+
+    nx = 10
+
+    coords = np.array([[0.75], [0.5], [0.25], [0.125]])
+    nd = len(coords)
+
+    A, b, mesh, V = create_assembled_problem(nx, COMM_WORLD)
+
+    im = InterpolationMatrix(V, coords)
+
+    im_2 = assemble(im)
+
+    assert im_2.is_assembled
+
+    interp_expected = create_interp(mesh, V)
+
+    imin, imax = im_2.interp.getOwnershipRange()
+
+    for i in range(imin, imax):
+        for j in range(nd):
+            assert_allclose(im_2.interp.getValue(i, j), interp_expected[i,j], atol = 1.e-10)
+
+    im.destroy()
+
+def test_assemble_firedrake():
+    "test assemble with a firedrake object"
+
+    # assemble firedrake matrix
+
+    nx = 10
+
+    _, _, mesh, V = create_assembled_problem(nx, COMM_WORLD)
+
+    u = TrialFunction(V)
+    v = TestFunction(V)
+    f = Function(V)
+    x = SpatialCoordinate(mesh)
+    f.interpolate((4.*pi*pi)*sin(x[0]*pi*2))
+    a = (dot(grad(v), grad(u))) * dx
+    L = f * v * dx
+    bc = DirichletBC(V, 0., "on_boundary")
+    A = assemble(a, bcs = bc)
+
+    assert isinstance(A, Matrix)
