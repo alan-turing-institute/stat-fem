87935240c6e8ca66c828a60de5a81b9d992988a6
edaub <edaub@turing.ac.uk>
2020-03-20 16:35:41 +0000

    added docstrings for assembly, covariance functions, and estimation functions

--- stat_fem/CovarianceFunctions.py
+++ stat_fem/CovarianceFunctions.py
@@ -2,7 +2,32 @@ import numpy as np
 from scipy.spatial.distance import cdist
 
 def sqexp(x1, x2, sigma, l):
-    "returns squared exponential covariance function. note parameters are assumed to be on log scale"
+    """
+    Squared Exponential Covariance Function
+
+    Returns squared exponential covariance function computed at all pairs of values in coordinate
+    values ``x1`` and ``x2``. This returns a 2D array with the first axis of the same length as
+    the first axis of ``x1``, and the second axis of the same length as the first axis of ``x2``.
+    If ``x1`` or ``x2`` are 1D, it broadcasts to 2D assuming that the first axis has length 1.
+
+    Note parameters are assumed to be on log scale. ``sigma`` is the overall covariance scale,
+    where ``exp(sigma)`` is the standard deviation, and ``l`` determines the correlation length,
+    where ``exp(l)`` is the length scale.
+
+    :param x1: first set of input coordinates. Must be a 1D or 2D numpy array (if 1D, it assumes the
+               first axis has length 1).
+    :type x1: ndarray
+    :param x2: second set of input coordinates. Must be a 1D or 2D numpy array (if 1D, it assumes the
+               first axis has length 1).
+    :type x2: ndarray
+    :param sigma: Covariance parameter on a logarithmic scale (``exp(sigma)`` gives the standard
+                  deviation).
+    :type sigma: float
+    :param l: Correlation length on a logarithmic scale (``exp(l)`` gives the standard deviation).
+    :type l: float
+    :returns: Squared Exponential Covariance Matrix, a numpy array with shape ``(x1.shape[0], x2.shape[1])``
+    :rtype: ndarray
+    """
 
     x1 = np.array(x1)
     x2 = np.array(x2)
@@ -12,7 +37,34 @@ def sqexp(x1, x2, sigma, l):
     return np.exp(2.*sigma)*np.exp(-0.5*r2*np.exp(-2.*l))
 
 def sqexp_deriv(x1, x2, sigma, l):
-    "returns partial derivatives of the squared exponential covariance function"
+    """
+    Squared Exponential Covariance Function Derivative
+
+    Returns the gradient of the squared exponential covariance function computed at all pairs of
+    values in coordinate values ``x1`` and ``x2``. This returns a 3D array with the first axis of
+    length 2 (representing the two derivative components), the second axis is of the same length as
+    the first axis of ``x1``, and the third axis of the same length as the first axis of ``x2``.
+    If ``x1`` or ``x2`` are 1D, it broadcasts to 2D assuming that the first axis has length 1.
+
+    Note parameters are assumed to be on log scale. ``sigma`` is the overall covariance scale,
+    where ``exp(sigma)`` is the standard deviation, and ``l`` determines the correlation length,
+    where ``exp(l)`` is the length scale.
+
+    :param x1: first set of input coordinates. Must be a 1D or 2D numpy array (if 1D, it assumes the
+               first axis has length 1).
+    :type x1: ndarray
+    :param x2: second set of input coordinates. Must be a 1D or 2D numpy array (if 1D, it assumes the
+               first axis has length 1).
+    :type x2: ndarray
+    :param sigma: Covariance parameter on a logarithmic scale (``exp(sigma)`` gives the standard
+                  deviation).
+    :type sigma: float
+    :param l: Correlation length on a logarithmic scale (``exp(l)`` gives the standard deviation).
+    :type l: float
+    :returns: Squared Exponential Covariance Matrix gradient, a numpy array with shape
+              ``(2, x1.shape[0], x2.shape[1])``
+    :rtype: ndarray
+    """
 
     x1 = np.array(x1)
     x2 = np.array(x2)
@@ -25,7 +77,34 @@ def sqexp_deriv(x1, x2, sigma, l):
     return deriv
 
 def sqexp_hessian(x1, x2, sigma, l):
-    "returns hessian of the squared exponential covariance function"
+    """
+    Squared Exponential Covariance Function Hessian
+
+    Returns the Hessian of the squared exponential covariance function computed at all pairs of
+    values in coordinate values ``x1`` and ``x2``. This returns a 4D array with the first two axes of
+    length 2 (representing the two derivative components), the third axis is of the same length as
+    the first axis of ``x1``, and the fourth axis of the same length as the first axis of ``x2``.
+    If ``x1`` or ``x2`` are 1D, it broadcasts to 2D assuming that the first axis has length 1.
+
+    Note parameters are assumed to be on log scale. ``sigma`` is the overall covariance scale,
+    where ``exp(sigma)`` is the standard deviation, and ``l`` determines the correlation length,
+    where ``exp(l)`` is the length scale.
+
+    :param x1: first set of input coordinates. Must be a 1D or 2D numpy array (if 1D, it assumes the
+               first axis has length 1).
+    :type x1: ndarray
+    :param x2: second set of input coordinates. Must be a 1D or 2D numpy array (if 1D, it assumes the
+               first axis has length 1).
+    :type x2: ndarray
+    :param sigma: Covariance parameter on a logarithmic scale (``exp(sigma)`` gives the standard
+                  deviation).
+    :type sigma: float
+    :param l: Correlation length on a logarithmic scale (``exp(l)`` gives the standard deviation).
+    :type l: float
+    :returns: Squared Exponential Covariance Matrix Hessian, a numpy array with shape
+              ``(2, 2, x1.shape[0], x2.shape[1])``
+    :rtype: ndarray
+    """
 
     x1 = np.array(x1)
     x2 = np.array(x2)
@@ -41,7 +120,24 @@ def sqexp_hessian(x1, x2, sigma, l):
     return hess
 
 def calc_r2(x1, x2):
-    "compute squared distance between points"
+    """
+    Compute Squared Distance between all pairs of points in two arrays
+
+    Wrapper to the Scipy ``cdist`` function squared for computing squared distances
+    between pairs of points in two arrays. This returns a 2D array with the first axis
+    of the same length as the first axis of ``x1``, and the second axis of the same length
+    as the first axis of ``x2``. If ``x1`` or ``x2`` are 1D, it broadcasts to 2D assuming
+    that the first axis has length 1.
+
+    :param x1: first set of input coordinates. Must be a 1D or 2D numpy array (if 1D, it assumes the
+               first axis has length 1).
+    :type x1: ndarray
+    :param x2: second set of input coordinates. Must be a 1D or 2D numpy array (if 1D, it assumes the
+               first axis has length 1).
+    :type x2: ndarray
+    :returns: Squared distance matrix, a numpy array with shape ``(x1.shape[0], x2.shape[1])``
+    :rtype: ndarray
+    """
 
     x1 = np.array(x1)
     x2 = np.array(x2)
--- stat_fem/estimation.py
+++ stat_fem/estimation.py
@@ -1,14 +1,55 @@
 import numpy as np
 from scipy.optimize import minimize
-from scipy.linalg import cho_factor, cho_solve, LinAlgError
 from firedrake import COMM_SELF, COMM_WORLD
 from .LinearSolver import LinearSolver
-from .solving import solve_prior_covariance
-from functools import partial
 from mpi4py import MPI
 
 def estimate_params_MAP(A, b, G, data, priors=[None, None, None], start=None, ensemble_comm=COMM_SELF, **kwargs):
-    "use maximum a posteriori to estimate parameters using LBFGS, returning a fit LinearSolver object"
+    """
+    Estimate model hyperparameters using MAP estimation
+
+    This function uses maximum a posteriori estimation to fit parameters for a Statistical FEM model.
+    The function is a wrapper to the Scipy LBFGS function to minimize the marginal log posterior,
+    returning a fit ``LinearSolver`` object. This allows re-use of the cached Forcing Covariance
+    function solves for each sensor, which greatly improves efficiency of the computation.
+
+    Priors on the three hyperparameters can be specified by passing a list of ``Prior``-derived
+    objects. The priors are over the values as passed directly to all functions (i.e. while still
+    on a logarithmic scale). If no priors are provided, then an uninformative prior is assumed
+    and estimation is based on the marginal log-likelihood.
+
+    The computations to solve for the forcing covariance can be carried out in parallel by passing
+    a Firedrake Ensemble communicator for the ``ensemble_comm`` keyword argument. All other
+    computations are done on the root process and broadcast to all other processes to
+    ensure that all processes have the same final minimum value when the computation terminates.
+
+    :param A: FEM stiffness matrix, must be a Firedrake Matrix class.
+    :type A: Matrix
+    :param b: FEM RHS vector, must be a Firedrake Function or Vector
+    :type b: Function or Vector
+    :param G: Forcing Covariance matrix, must be a ``ForcingCovariance`` class.
+    :type G: ForcingCovariance
+    :param data: Observational data object, must be a ``ObsData`` class.
+    :type data: ObsData
+    :param priors: List of prior objects over hyperparameters. Must be a list of length 3
+                   containing ``Prior``-derived objects or ``None`` for uninformative priors.
+                   Optional, default is ``[None, None, None]`` (uninformative priors on all
+                   parameters).
+    :type priors: list
+    :param start: Starting point for minimization routine. Must be a numpy array of length 3 or
+                  ``None`` if the starting point is to be drawn randomly. Optional, default is
+                  ``None``.
+    :type start: None or ndarray
+    :param ensemble_comm: MPI communicator for ensemble parallelism created from a Firedrake
+                          ``Ensemble`` object. This controls how the solves over all sensors
+                          are parallelized. Optional, default value is ``COMM_SELF`` indicating
+                          that forcing covariance solves are not parallelized.
+    :type enemble_comm: MPI Communicator
+    :param kwargs: Additional keyword arguments to be passed to the minimization routine.
+    :returns: A LinearSolver object with the hyperparameters set to the MAP/MLE value. To extract
+              the actual parameters, use the ``params`` attribute.
+    :rtype: LinearSolver
+    """
 
     ls = LinearSolver(A, b, G, data, priors, ensemble_comm)
 
